9d654e10009eebdf5aa5369d3f419c75e391bb99
Jonathan Lipps <jlipps@gmail.com>
2015-04-09 15:49:24 -0700

    consolidate transpiling with appium-gulp-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,6 @@
 "use strict";
 
 var gulp = require('gulp')
-  , merge = require('merge-stream')
-  , traceur = require('gulp-traceur');
+  , boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-gulp.task('default', function () {
-  var traceurOpts = {
-    asyncFunctions: true,
-    blockBinding: true,
-    modules: 'commonjs',
-    annotations: true,
-    arrayComprehension: true
-  };
-  var lib = gulp.src('lib/es6/**/*.js')
-                .pipe(traceur(traceurOpts))
-                .pipe(gulp.dest('lib/es5'));
-  var test = gulp.src('test/es6/**/*.js')
-                 .pipe(traceur(traceurOpts))
-                 .pipe(gulp.dest('test/es5'));
-  return merge(lib, test);
-});
-
-
+boilerplate({build: "Mochawait", jscs: false});
